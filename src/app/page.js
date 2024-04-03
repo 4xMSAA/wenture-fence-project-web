a1a7817b96d21c754c53695da284ea338347ffc0
Mia Soomre <msaa@miyaow.net>
2024-04-03 13:22:04 +0300

    use uppercase for constants

--- src/app/page.js
+++ src/app/page.js
@@ -2,25 +2,23 @@
 import React from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
-const path = "unity/Build/"
-const unityBuildName = "UnityFence"
+const PATH = "unity/Build/"
+const UNITY_BUILD_NAME = "UnityFence"
 
-function UnityInput() {
-
-}
 
 export default function App() {
   const { unityProvider } = useUnityContext({
-    loaderUrl: `${path}${unityBuildName}.loader.js`,
-    dataUrl: `${path}${unityBuildName}.data`,
-    frameworkUrl: `${path}${unityBuildName}.framework.js`,
-    codeUrl: `${path}${unityBuildName}.wasm`,
+    loaderUrl: `${PATH}${UNITY_BUILD_NAME}.loader.js`,
+    dataUrl: `${PATH}${UNITY_BUILD_NAME}.data`,
+    frameworkUrl: `${PATH}${UNITY_BUILD_NAME}.framework.js`,
+    codeUrl: `${PATH}${UNITY_BUILD_NAME}.wasm`,
   });
 
   return (
     <div id="app" style={{position: "absolute", width: "100vw", height: "100vh"}}>
-      <UnityInput>
-      </UnityInput>
+      <span style={{margin: "1rem 0 0 1rem", }}>
+
+      </span>
       <Unity unityProvider={unityProvider} style={{width: "100vw", height:"100vh", verticalAlign: "top"}} />
     </div>
   );
