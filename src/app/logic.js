bdda7018c10b9f953ec70439c065c0390c0956a3
Mia Soomre <msaa@miyaow.net>
2024-04-03 17:24:15 +0300

    mindbroken react and web facing logic

---
+++ src/app/logic.js
@@ -0,0 +1,72 @@
+const DEFAULT_FENCE_LENGTH = 1
+const DEFAULT_FENCE_ANGLE = 0
+const DEFAULT_FENCE_COUNT = 4
+
+const [MIN_ANGLE, MAX_ANGLE] = [0, 180]
+const MIN_LENGTH = 0.01
+
+export class Fence {
+    #length = DEFAULT_FENCE_LENGTH
+    #angle = DEFAULT_FENCE_ANGLE
+
+    constructor(length = DEFAULT_FENCE_LENGTH, angle = DEFAULT_FENCE_ANGLE) {
+        this.#length = length
+        this.#angle = angle
+    }
+
+    get length() {
+        return this.#length
+    }
+
+    set length(value) {
+        this.#length = Math.max(value, MIN_LENGTH)
+    }
+
+    get angle() {
+        return this.#angle
+    }
+
+    set angle(value) {
+        this.#angle = Math.min(Math.max(value, MIN_ANGLE), MAX_ANGLE) 
+    }
+}
+
+export default class Logic {
+
+    constructor(fenceCount = DEFAULT_FENCE_COUNT) {
+        this.fences = []
+
+        for (let i = 0; i < fenceCount; i++) {
+            this.addFence()
+        }
+    }
+
+    addFence(length, angle) {
+        this.fences.push(new Fence(length, angle))
+    }
+    addFenceUntilAtCount(count) {
+        for (let i = this.fences.length; i < count; i++) {
+            this.addFence()
+        }
+    }
+
+    
+    removeFence() {
+        this.fences.pop()
+    }
+
+    removeFenceUntilAtCount(count) {
+        for (let i = this.fences.length; i > count; i--) {
+            this.removeFence()
+        }
+    }
+
+    setFenceCount(count) {
+        this.addFenceUntilAtCount(count)
+        this.removeFenceUntilAtCount(count)
+    }
+
+    syncToUnity() {
+        // send out JSON on sendMessage
+    }
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,60 @@
 'use client'
-import React from "react";
+import { React, useState } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
+import Logic from "./logic"
 
 const PATH = "unity/Build/"
 const UNITY_BUILD_NAME = "UnityFence"
 
 
+const logic = new Logic()
+
+const FenceList = ({fencesList, setFencesList, logic}) => {
+    const list = fencesList.map((fence, i) => {
+        function onChangeLength(e) {
+            logic.fences[i].length = parseFloat(e.target.value)
+            setFencesList(logic.fences.slice())
+        }
+        function onChangeAngle(e) {
+            logic.fences[i].angle = parseFloat(e.target.value)
+            setFencesList(logic.fences.slice())
+        }
+
+        return <li>
+            <input type="number" value={fence.length} step="0.01" min="0.01" onChange={onChangeLength} />
+            <input type="number" value={fence.angle} step="1" min="0" max="180" onChange={onChangeAngle} />
+        </li>
+    })
+
+    return (
+        <ol>{list}</ol>
+    )
+}
+
 export default function App() {
-  const { unityProvider } = useUnityContext({
-    loaderUrl: `${PATH}${UNITY_BUILD_NAME}.loader.js`,
-    dataUrl: `${PATH}${UNITY_BUILD_NAME}.data`,
-    frameworkUrl: `${PATH}${UNITY_BUILD_NAME}.framework.js`,
-    codeUrl: `${PATH}${UNITY_BUILD_NAME}.wasm`,
-  });
+    const { unityProvider } = useUnityContext({
+        loaderUrl: `${PATH}${UNITY_BUILD_NAME}.loader.js`,
+        dataUrl: `${PATH}${UNITY_BUILD_NAME}.data`,
+        frameworkUrl: `${PATH}${UNITY_BUILD_NAME}.framework.js`,
+        codeUrl: `${PATH}${UNITY_BUILD_NAME}.wasm`,
+    });
 
-  return (
-    <div id="app" style={{position: "absolute", width: "100vw", height: "100vh"}}>
-      <span style={{margin: "1rem 0 0 1rem", }}>
+    const [fenceCount, setFenceCount] = useState(logic.fences.length)
+    const [fencesList, setFencesList] = useState(logic.fences.slice())
 
-      </span>
-      <Unity unityProvider={unityProvider} style={{width: "100vw", height:"100vh", verticalAlign: "top"}} />
-    </div>
-  );
+    function onChangeFenceCount(e) {
+        logic.setFenceCount(parseInt(e.target.value))
+        setFenceCount(logic.fences.length)
+        setFencesList(logic.fences.slice())
+    }
+
+    return (
+        <div id="app" style={{position: "absolute", width: "100vw", height: "100vh"}}>
+        <span style={{position: "absolute", margin: "1rem 0 0 1rem"}}>
+            <input type="number" min="0" value={fenceCount} onChange={onChangeFenceCount}/>
+            <FenceList fencesList={fencesList} setFencesList={setFencesList} logic={logic}/>
+        </span>
+        <Unity unityProvider={unityProvider} style={{width: "100vw", height:"100vh", verticalAlign: "top"}} />
+        </div>
+    );
 }
